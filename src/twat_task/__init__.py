c4ebc862b328e5887c0f665f2f2bbeb6b6a0e727
twardoch <adam@twardoch.com>
2025-02-13 22:56:22 +0100

    v1.7.0

--- src/twat_task/__init__.py
+++ src/twat_task/__init__.py
@@ -17,13 +17,8 @@ Example:
 
 from importlib import metadata
 
-from .__version__ import version as __version__
-from .task import (
-    VideoTranscript,
-    extract_audio_task,
-    generate_transcript_task,
-    process_video_flow,
-)
+from twat_task.__version__ import version as __version__
+from twat_task.task import VideoTranscript, extract_audio_task, generate_transcript_task, process_video_flow
 
 __all__ = [
     "VideoTranscript",
